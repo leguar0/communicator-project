bfb2f240e9e79aded6a07acd62ced968352c1b8d
leguar0 <ascelarkas@gmail.com>
2024-03-23 20:05:39 +0100

    I created folders client and server

--- server/Server.py
+++ server/Server.py
@@ -1,1 +1,41 @@
-#test
+from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+app = FastAPI()
+users = {}
+unread_messages = {}
+
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+
+class Message(BaseModel):
+    id_sender: int
+    id_reciver: int
+    message: str
+    data: str
+    is_read: bool
+
+@app.get("/current_users")
+def current_users():
+    return users
+
+@app.get("/unread_messages_from_user/{id_user}")
+def get_unread_messages_from_user(id_user):
+    pass
+
+@app.get("/unread_messages")
+def get_unread_messages():
+    pass
+
+@app.post("/register_user")
+def register_user(name: str, surname: str):
+    pass
+
+@app.post("/send_message")
+def send_message(message: Message):
+    pass
+
+
