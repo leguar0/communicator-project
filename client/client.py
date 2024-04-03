4dac63b3b89fb718cef0d6682c0790fcc951d6c1
Bartosz Kowalski <106326951+Bezio1292@users.noreply.github.com>
2024-04-03 13:25:48 +0200

    Add more code for testing purpose. Allow to send messages.

--- client/client.py
+++ client/client.py
@@ -13,12 +13,26 @@ user_json = {
 url = 'http://127.0.0.1:8000/register_user'
 result = requests.post(url, json = user_json)
 print(result.status_code, result.json())
+cur_user_id = result.json()["id"]
 
+time.sleep(2)
 url = 'http://127.0.0.1:8000/current_users'
+print("Current users:")
+print(requests.get(url).json())
 
+message = input("Message:")
+id_reciver = int(input("to user: "))
+if id_reciver != 0:
+    url = f'http://127.0.0.1:8000/send_message'
+    requests.post(url, json = {"id_sender":cur_user_id, "id_reciver":id_reciver,"message":message})
+
+
+url = f'http://127.0.0.1:8000/unread_messages?id_user={cur_user_id}'
 try:
     while True:
-        print(requests.get(url).json())
+        res = requests.get(url).json()
+        if(len(res) > 0):
+            print(res)
         time.sleep(2)
 except KeyboardInterrupt:
     pass
