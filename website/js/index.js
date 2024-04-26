8447c0246cf8c43d56c50461b09a7f4dea3b9ec5
Bartosz Kowalski <106326951+Bezio1292@users.noreply.github.com>
2024-04-26 21:13:37 +0200

    Add simple script which fetch users in home page.

---
+++ website/js/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const usersHolder = document.querySelector("#online-users-holder");
+async function get_current_users(){
+    let url = `http://localhost:8000/current_users`;
+    let response = await fetch(url);
+    if(response.ok){
+        let resp_data = await response.json();
+        console.log(resp_data)
+
+        for(let i =0;i<resp_data.length;++i){
+            let a = document.createElement("a");
+            a.id = "user_id_" + resp_data[i].id;
+
+            let div = document.createElement("div");
+            div.innerHTML = resp_data[i].name;
+            div.classList.add("user-holder");
+            
+            a.appendChild(div)
+            
+            usersHolder.appendChild(a);
+        }
+
+        
+        //return resp_data.name
+    }
+}
+
+
+get_current_users();
