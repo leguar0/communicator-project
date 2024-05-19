0bcd3bc1905d8d52785b16049160449627339c7e
Bartosz Kowalski <106326951+Bezio1292@users.noreply.github.com>
2024-05-19 13:24:49 +0200

    Remove unnecessary INER JOIN from query.

--- server/server.py
+++ server/server.py
@@ -82,7 +82,7 @@ async def get_unread_messages_from_user(cur_id_user, id_user):
 
 @app.get("/count_unread_messages_from_user")
 async def get_count_unread_messages_from_user(id_sender, id_receiver):
-    cur.execute('SELECT COUNT(*) as total_messages FROM messages INNER JOIN users ON users.id_user = messages.id_sender WHERE id_sender = ? AND id_receiver = ? AND is_read = 0', (id_sender, id_receiver))
+    cur.execute('SELECT COUNT(*) as total_messages FROM messages WHERE id_sender = ? AND id_receiver = ? AND is_read = 0', (id_sender, id_receiver))
     fetch = cur.fetchone()
     return fetch[0]
 
