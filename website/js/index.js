d349c993fbaa286a7c356e9d686f5e9dcb558534
Bartosz Kowalski <106326951+Bezio1292@users.noreply.github.com>
2024-05-19 13:28:13 +0200

    Add option to display the count of unread messages from user.

--- website/js/index.js
+++ website/js/index.js
@@ -23,14 +23,34 @@ async function get_current_users(){
             a.href=`chat.html?id=${resp_data[i].id}&fullname=${resp_data[i].name} ${resp_data[i].surname}`;
             a.id = "user_id_" + resp_data[i].id;
 
-            let div = document.createElement("div");
-            div.innerHTML = `${resp_data[i].name} ${resp_data[i].surname}`;
-            div.classList.add("user-holder");
+            let div_container = document.createElement("div");
+            div_container.classList.add("user-holder");
             
-            a.appendChild(div)
+            let div_name = document.createElement("div");
+            div_name.innerHTML = `${resp_data[i].name} ${resp_data[i].surname}`;
+            div_container.appendChild(div_name);
+
+            let div_msg_count = document.createElement("div");
+            div_msg_count.classList.add("msg-count");
+            div_container.appendChild(div_msg_count);
+
+            a.appendChild(div_container);
             
             usersHolder.appendChild(a);
+
+            count_unread_messages(resp_data[i].id)
         }
     }
 }
+
+async function count_unread_messages(is_sender){
+    let url = `http://localhost:8000/count_unread_messages_from_user?id_sender=${is_sender}&id_receiver=${USER_ID}`
+    let response = await fetch(url);
+    if(response.ok){
+        let resp_data = await response.json();
+        if(resp_data == 0) return;
+        document.querySelector(`#user_id_${is_sender} .msg-count`).innerHTML += `📩${resp_data}`;
+    }
+}
+
 get_current_users();
